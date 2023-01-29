df32809e8e3b16a82d4124f158805d11930b40bf
rootGuillaume <guillaumegasmi@protonmail.com>
2023-01-29 23:48:38 +0100

    Test argument parser

--- azstudenv/common/arguments.py
+++ azstudenv/common/arguments.py
@@ -6,39 +6,56 @@ class ArgsParser:
     """."""
 
     def __init__(self):
+
         parser = argparse.ArgumentParser()
 
-        self.subparser = parser.add_subparsers(help="Commands")
 
-        apply = self.subparser.add_parser("apply")
-        apply.set_defaults(func=print("Applying..."))
 
-        destroy = self.subparser.add_parser("destroy")
-        destroy.set_defaults(func=print("Destroying..."))
-
-        build = self.subparser.add_parser("build")
-
-        build.add_argument(
+        parser.add_argument(
             "-i", 
             "--image", 
             help="ISO images",
             required=True
         )
-        build.add_argument(
+        parser.add_argument(
             "-p", 
             "--poc", 
             help="POC name",
             required=True
         )
-        build.add_argument(
+        parser.add_argument(
             "-a", 
             "--amount", 
             help="Amout of vms to create",
             required=True
         )
 
+
+        self.subparser = parser.add_subparsers(title="Applying", help="Commands", dest="apply")
+
+        parser_build = self.subparser.add_parser(
+                "build", 
+                help="Build AzStuden configuration", 
+                add_help=False, 
+                parents=[parser]
+        )
+
+        parser_apply = self.subparser.add_parser(
+                "apply", 
+                help="Applying Terraform plan", 
+                add_help=False, 
+                parents=[parser]
+        )
+
+        parser_destroy = self.subparser.add_parser(
+                "destroy", 
+                help="Destroying Terraform plan", 
+                add_help=False, 
+                parents=[parser]
+        )
+
         args = parser.parse_args()
 
-
+        print(args)
 
 ArgsParser()
