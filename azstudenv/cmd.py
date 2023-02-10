9fcaf7d9113db75f5258f0e22b5f6f517cfa730b
Guillaume <guillaumegasmi@protonmail.com>
2023-02-10 23:26:49 +0100

    Update tf cmd

--- azstudenv/cmd.py
+++ azstudenv/cmd.py
@@ -1,3 +1,4 @@
+from typing import Literal
 import subprocess
 import typer
 import time
@@ -9,10 +10,9 @@ cmd1 = ["terraform", "-chdir=terraform/", "apply", "--auto-approve"],#, "-no-col
 cmd2 = ["ls"]
 
 
-
-def run(cmd:str):
-
-    count = 0
+def cmd(option: Literal["apply", "destroy"]) -> None:
+    """
+    """
 
     with Progress(
         SpinnerColumn(),
@@ -21,33 +21,18 @@ def run(cmd:str):
     ) as progress:
 
         with subprocess.Popen(
-            ["terraform", cmd, "-auto-approve", "-no-color"], 
+            ["terraform", option, "-auto-approve", "-no-color"], 
             stdout=subprocess.PIPE, 
             stderr=subprocess.PIPE,
         ) as process:
-            progress.add_task(description="Creating AzStudenv infrastructure...", total=None)
-
+            task = progress.add_task(
+                description=f"{option.capitalize()}ing AzStudenv infrastructure...", 
+                total=None
+            )
 
             for line in process.stdout:
                 line = line.decode("utf-8")
 
-                if not line and not count:
-                    message = "Terraform error while executing. Refer to the error message above."
-                    #Console.error("*.tf", message)
-                    return
-
-                if not line and count:
-                    break
-
                 if "complete after" in line:
-                    progress.update(
-                        task_id=0,
-                        description="Resource group created.", 
-                        total=None
-                    )
                     print(line)
-                    count += 1
 
-
-run("apply")
-run("destroy")
