4d5b0b107f439aa6a7190129798024670c677b9e
0xGuillaume <guillaumegasmi@protonmail.com>
2023-02-19 21:39:35 +0100

    Writed all class/function docstrings

--- azstudenv/common/config.py
+++ azstudenv/common/config.py
@@ -1,26 +1,34 @@
+"""Module handling configuration setup."""
 import re
 import os
-import rich
 import string
 from pathlib import Path
 from typing import Union
-#from files import Yaml, Console
 from common.files import Yaml, Console
 
 
-CONFIG_FILE = "/home/jimbo/projects/AzStudenv/azstudenv/terraform/config.yaml"
+CONFIG_FILE = "/home/guillaume/projects/AzStudenv/azstudenv/terraform/config.yaml"
 
 
 class ConfigTest:
-    """
+    """Configurations unit tests.
+
+    Several tests to check if configuration file
+    key are compliant. 
+    Those tests run before appending to the configuration file
+    and before running AzStudenv commands.
     """
 
     def __init__(self) -> None:
-        """"""
+        """Initialize ConfigTest class."""
 
 
     def is_username(self, username:str) -> bool:
-        """Check if the given admin username is azure compliant."""
+        """Check if the given admin username is azure compliant.
+        
+        Args:
+            username: User's given username.
+        """
 
         authorized_chars = string.ascii_lowercase
         authorized_chars += string.ascii_uppercase
@@ -28,50 +36,71 @@ class ConfigTest:
         authorized_chars += "-_"
 
         banned_username = [
-                "administrator", "admin", "user", "user1", "test", "user2", "test1",
-                "user3", "admin1", "1", "123", "a", "actuser", "adm", "admin2", "aspnet",
-                "backup", "console", "david", "guest", "john", "owner", "root", "server",
-                "sql", "support", "support_388945a0", "sys", "test2", "test3", "user4", "user5"
+            "administrator", "admin", "user", "user1", "test", "user2", "test1",
+            "user3", "admin1", "1", "123", "a", "actuser", "adm", "admin2", "aspnet",
+            "backup", "console", "david", "guest", "john", "owner", "root", "server",
+            "sql", "support", "support_388945a0", "sys", "test2", "test3", "user4", "user5"
         ]
 
         if len(username) < 1 or len(username) > 64:
-            message = ("Admin username value must be between 1 and 64 characters"
-                f"long [Length: {len(username)}].")
+            message = (
+                "Admin username value must be between 1 and 64 characters"
+                f"long [Length: {len(username)}]."
+            )
             Console.error(message)
             return False
 
         if username in banned_username:
-            message = f"Given admin username : [cyan bold]{username}[/cyan bold] is a banned value by Azure."
+            message = (
+                f"Given admin username : [cyan bold]{username}[/cyan bold] "
+                "is a banned value by Azure."
+            )
             Console.error(message)
             return False
 
-        for count, char in enumerate(username):
+        for char in username:
             if char not in authorized_chars:
-                message = (f"[[cyan bold]{char}[/cyan bold] in [cyan bold]{username}[/cyan bold] [Position: {count + 1}]"
-                    "is not an authorized character for admin_username.")
+                message = (
+                    f"[[cyan bold]{char}[/cyan bold] in [cyan bold]{username}[/cyan bold] "
+                    "[Position: {count + 1}]"
+                    "is not an authorized character for admin_username."
+                )
                 Console.error(message)
                 return False
         return True
 
 
     def is_pocname(self, name:str) -> bool:
-        """Check if poc name is alphanumeric only."""
+        """Check if poc name is alphanumeric only.
+
+        Args:
+            name: User's given proof of concept name.
+        """
 
         if not name.isalpha():
-            message = (f"[cyan bold]{name}[/cyan bold] is not a valid name. "
-                "Only letters are allowed for naming POC.")
+            message = (
+                f"[cyan bold]{name}[/cyan bold] is not a valid name. "
+                "Only letters are allowed for naming POC."
+            )
             Console.error(message)
 
         return name.isalpha()
 
 
     def is_subscription(self, id_:str) -> bool:
-        """Check wether or not Azure subscription ID is well formatted"""
+        """Check wether or not Azure subscription ID is well formatted.
+        
+        Args:
+            id_: User's Azure subscription.
+        """
 
-        pattern = "[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}" 
+        pattern = "[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}"
 
         if not bool(re.match(pattern, id_)):
-            message = f"Given Azure subscription [cyan bold]{id_}[/cyan bold] does not respect Azure subscription pattern."
+            message = (
+                f"Given Azure subscription [cyan bold]{id_}[/cyan bold] "
+                "does not respect Azure subscription pattern."
+            )
             Console.error(message)
             return False
 
@@ -79,26 +108,56 @@ class ConfigTest:
 
 
     def is_sshkey(self, path:Union[str, os.PathLike]) -> bool:
-        """Check wether or not ssh_key path exists"""
+        """Check wether or not ssh_key path exists.
+
+        Args:
+            path: User's ssh public key path.
+        """
 
         if not Path(path).exists():
             message = f"SSH Public key [cyan bold]{path}[/cyan bold] does not exist."
             Console.error(message)
             return False
-        
+
+        return True
+
+    def is_init(self) -> bool:
+        """Check wether or not config has been init (is not empty)."""
+
+        config = bool(Yaml.read(CONFIG_FILE))
+
+        if not config:
+            message = (
+                "Configuration file [cyan bold]config.yaml[/cyan bold] " 
+                "is empty or has not been set. "
+                "Run [cyan bold]config[/cyan bold] command in order to initate "
+                "configuration file."
+            )
+            Console.error(message)
+            return False
+
         return True
 
 
 
 class Config(ConfigTest):
-    """
+    """Default configuration.
+
+    Config is used to store default configuration
+    YAML pattern and allowed images used to 
+    setup instances.
+
+    Config is also used to interact with `config.yaml` file:
+        - Initiate configuration file.
+        - Fill or append new configuration.
+        - Reset configuration file.
     """
 
     def __init__(self) -> None:
-        """"""
+        """Initialize Config clas."""
 
     def _default(self) -> dict:
-        """Original configuration"""
+        """Default configuration pattern."""
 
         default = {
             'idrsa': None, 
@@ -110,7 +169,12 @@ class Config(ConfigTest):
             'vm': {
                 'size': 'Standard_B1s',
                 'admin_username': None, 
-                'image': {'offer': None, 'publisher': None, 'sku': None, 'version': None}
+                'image': {
+                    'offer': None, 
+                    'publisher': None, 
+                    'sku': None, 
+                    'version': None
+                }
             }
         }
 
@@ -118,12 +182,28 @@ class Config(ConfigTest):
 
 
     def _image(self) -> dict:
-        """Return chosen image"""
+        """Return chosen image."""
 
         images = {
-            'debian': {'offer': 'debian-11', 'publisher': 'debian', 'sku': '11', 'version': 'latest'}, 
-            'rhel': {'offer': 'RHEL', 'publisher': 'RedHat', 'sku': '86-gen2', 'version': 'latest'}, 
-            'ubuntu': {'offer': '0001-com-ubuntu-server-focal', 'publisher': 'canonical', 'sku': '20_04-lts-gen2', 'version': 'latest'}
+            'debian': {
+                'offer': 'debian-11', 
+                'publisher': 'debian', 
+                'sku': '11', 
+                'version': 'latest'
+            },
+            'rhel': {
+                'offer': 'RHEL', 
+                'publisher': 'RedHat', 
+                'sku': '86-gen2', 
+                'version': 
+                'latest'
+            },
+            'ubuntu': {
+                'offer': '0001-com-ubuntu-server-focal', 
+                'publisher': 'canonical', 
+                'sku': '20_04-lts-gen2', 
+                'version': 'latest'
+            }
         }
 
         return images
@@ -137,10 +217,17 @@ class Config(ConfigTest):
 
     def reset(self) -> None:
         """Reset configuration file with default model."""
-    
+
 
     def fill_config_infra(self, instances:dict, image:str, pocname:str, suffix:str) -> None:
-        """Fill the user configuration in config.yaml."""
+        """Fill the infrastructure configuration in config.yaml.
+        
+        Args:
+            instances: Dictionnary of instances (hostname:image) to create.
+            image: Detailed image (ISO) chosen.
+            pocname: POC name chosen.
+            suffix: 3 capital letters based on firt 3 letters of the POC name.
+        """
 
         config = Yaml.read(CONFIG_FILE)
 
@@ -153,7 +240,13 @@ class Config(ConfigTest):
 
 
     def fill_config_user(self, subscription:str, sshkey:str, username:str) -> None:
-        """Fill the infrastructure configuration in config.yaml."""
+        """Fill User's configuration in config.yaml.
+        
+        Args:
+            subscription: User's Azure subscription.
+            sshkey: User's SSH key path.
+            username: Username used by user to connect instances.
+        """
 
         config = self._default()
         config["idrsa"] = sshkey
@@ -162,47 +255,65 @@ class Config(ConfigTest):
 
         Yaml.write(CONFIG_FILE, config)
 
-    
+
     def is_well_formated(self) -> bool:
         """Check if configuration is well formated."""
-        
+
         default = self._default()
         config = Yaml.read(CONFIG_FILE)
 
         main = config.keys() == default.keys()
-        vm = config["vm"].keys() == default["vm"].keys()
+        host = config["vm"].keys() == default["vm"].keys()
         image = config["vm"]["image"].keys() == default["vm"]["image"].keys()
 
-        return main == vm == image
+        return main == host == image
 
 
     def is_compliant(self) -> bool:
         """Check if configuration is compliant."""
 
-        config = Yaml.read(CONFIG_FILE)
-        fmt = self.is_well_formated(config)
+        #config = Yaml.read(CONFIG_FILE)
+        #fmt = self.is_well_formated(config)
+        return
 
-        #image = if config["image"] in self._i        
 
 
 class ConfigInfra(Config):
-    """"""
+    """Configuration of the infrastructure to create.
+
+    Setup the infrastructure configuration based on 
+    the user choices :
+        - Amount of instances.
+        - Images (rhel, ubuntu, debian).
+        - POC name.
+
+    Attributes:
+        amount: A string between : 1, 2 or 3.
+        image: A string between : rhel, ubuntu or debian.
+        pocname: A string indicating the infrastructure POC name.
+    """
 
     def __init__(
-        self, 
-        amount:str, 
-        image:str, 
+        self,
+        amount:str,
+        image:str,
         pocname:str
     ) -> None:
-        """Inits ConfigInfra class."""
+        """Initialize ConfigInfra class.
+        
+        Args:
+            amount: Amount of instances to create.
+            image: Image on wich instances will be based.
+            pocname: Name of the POC.
+        """
 
         self.amount     = amount
         self.image      = image
         self.pocname    = pocname
 
         # Check if config has already been settuped by ConfigUser
-        #if not Yaml.read(CONFIG_FILE):
-        #    self.init()
+        if not self.is_init():
+            return
 
         self.fill_config_infra(
             self._instances(),
@@ -211,17 +322,21 @@ class ConfigInfra(Config):
             self._suffix()
         )
 
-        Console.info("[green bold]✓[/green bold] [bold]Infrastructure configuration successfully setup![/bold]")
+        message = (
+            "[green bold]✓[/green bold] [bold]"
+            "Infrastructure configuration successfully setup![/bold]"
+        )
+        Console.info(message)
 
 
     def _pocname(self) -> str:
-        """Define poc name"""
+        """Define poc name."""
 
         if not self.is_pocname(self.pocname):
             return False
 
         return f"POC_{self.pocname.capitalize()}"
-        
+
 
     def _suffix(self) -> str:
         """Define poc name suffix in 3 capital letters."""
@@ -232,12 +347,12 @@ class ConfigInfra(Config):
     def _instances(self) -> dict:
         """Set hostname and ISO image for Az virtual machines."""
 
-        vms = int(self.amount)
+        hosts = int(self.amount)
         instances = {}
 
-        for vm in range(0, vms):
+        for host in range(0, hosts):
             image = self.image[:3].upper()
-            instance = f"AZUX{image}0{vm + 1}"
+            instance = f"AZUX{image}0{host + 1}"
             instances[instance] = self.image.value
 
         return instances
@@ -245,27 +360,44 @@ class ConfigInfra(Config):
 
 
 class ConfigUser(Config):
-    """
+    """Configuration of the user's settings.
+
+    Setup the configuration file with required
+    user's settings for Azure and Terraform needs:
+        - Azure subscription.
+        - SSH key path.
+        - Username.
+
+    Attributes:
+        subscription: A string indicating user's Azure subscription.
+        sshkey: A string indicating a SSH key path.
+        username: A string indicating a username.
     """
 
     def __init__(
-        self, 
-        subscription:str, 
-        sshkey:str, 
+        self,
+        subscription:str,
+        sshkey:str,
         username:str
     ) -> None:
-        """Inits ConfigUser class."""
+        """Initialize ConfigUser class.
+        
+        Args:
+            subscription: User's Azure subcription.
+            sshkey: User's sshkey path.
+            username: Username used on instances (username@host).
+        """
 
         self.subscription   = subscription
         self.sshkey         = sshkey
         self.username       = username
 
-        _subscription   = self._subscription() 
+        _subscription   = self._subscription()
         _sshkey         = self._sshkey()
         _username       = self._username()
 
         if (
-            _username 
+            _username
             and _sshkey
             and _subscription
         ):
@@ -278,20 +410,24 @@ class ConfigUser(Config):
                 _username
             )
 
-            Console.info("[green bold]✓[/green bold] [bold]User configuration successfully filled![/bold]")
+            message = (
+                "[green bold]✓[/green bold] [bold]"
+                "User configuration successfully filled![/bold]"
+            )
+            Console.info(message)
 
 
     def _username(self) -> str:
-        """Define Azure VM admin username"""
+        """Define Azure VM admin username."""
 
         if not self.is_username(self.username):
             return False
-    
+
         return self.username
 
-                        
+
     def _subscription(self) -> str:
-        """Fill Azure subscription"""
+        """Fill Azure subscription."""
 
         if not self.is_subscription(self.subscription):
             return False
@@ -300,36 +436,9 @@ class ConfigUser(Config):
 
 
     def _sshkey(self) -> str:
-        """Fill SSH public key"""
+        """Fill SSH public key."""
 
         if not self.is_sshkey(self.sshkey):
             return False
 
         return str(self.sshkey)
-
-
-
-
-
-
-if __name__ == "__main__":
-
-    ConfigUser(
-        "00000000-1234-0000-0000-000000000000", 
-        "/home/guillaume/.ssh/id_rsa.pub",
-        "jimbo"
-    )
-
-    """
-    i = ConfigInfra(
-        "2",
-        "rhel", 
-        "ansible"
-    )
-    """
-
-    #r = Config()
-    #r.init()
-
-    #a = Yaml.read(CONFIG_FILE)
-    #print(a)
